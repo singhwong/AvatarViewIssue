aa8b9b81c83601797de6e2574d9134baa0fcb4da
singhwong <singhwongwxg@hotmail.com>
2023-03-03 13:10:01 +0800

    avatarview issue

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -6,12 +6,18 @@ public partial class MainPage : ContentPage
     public MainPage()
     {
         InitializeComponent();
-        this.BindingContext = new MainVM();
     }
 
     private void OnCounterClicked(object sender, EventArgs e)
     {
-        
+        // avatarView.ImageSource = ImageSource.FromFile("dotnet_bot.png");
+        // avatarView.CornerRadius = new CornerRadius(75); ;
+    }
+    protected async override void OnAppearing()
+    {
+        base.OnAppearing();
+        await Task.Delay(1);
+        avatarView.ImageSource = ImageSource.FromFile("dotnet_bot.png");
     }
 }
 
